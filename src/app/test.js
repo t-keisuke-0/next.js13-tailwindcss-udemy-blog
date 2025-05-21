4f3ea8e0cc42e31c6f3bb41e88c7c82478dd5591
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-21 22:18:02 +0900

    20250521　まで

---
+++ src/app/test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const test = () => {
+  return (
+    <header>
+        test
+        <div>jafaifjap</div>
+        <p>dafdiaojf</p> 
+    </header>
+  )
+}
+
+export default test
