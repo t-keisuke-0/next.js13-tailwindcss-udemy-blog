d702d091e4e492c96b0e12874174d0a4516414c9
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-22 21:59:06 +0900

    画像の参照まで

---
+++ src/app/components/ArticleList.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const ArticleList = () => {
+  return (
+    <div>
+      <article>
+        <Link href="#">
+          {/* <Image 
+            src="https://pixabay.com/illustrations/pig-animal-sow-happy-pig-2147992"
+            alt="" 
+          /> */}
+        </Link>
+      </article>
+    </div>
+  )
+}
+
+export default ArticleList
