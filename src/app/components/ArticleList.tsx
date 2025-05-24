50299cf4ff6ea26e8f2ce84fd88ce81a7b18eaba
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-24 14:42:14 +0900

    画像投稿ページ作成まで

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -5,13 +5,67 @@ import React from 'react'
 const ArticleList = () => {
   return (
     <div>
-      <article>
-        <Link href="#">
-          {/* <Image 
-            src="https://pixabay.com/illustrations/pig-animal-sow-happy-pig-2147992"
-            alt="" 
-          /> */}
+      <article className='shadow my-4 flex flex-col'>
+        <Link href="#" className='hover:opacity-75'>
+          <Image
+            src="https://picsum.photos/1000/500?sig=1"
+            alt=""
+            width={1280}
+            height={300}
+          />
         </Link>
+        <div className='bg-white flex flex-col justify-start p-6'>
+          <Link href="#" className='text-blue-700 pb-4 font-bold'>
+            Technology
+          </Link>
+          <Link
+            href="#" 
+            className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className='text-sm pb-3 text-slate-900'>Published on 2023/07/15</p>
+          <Link href="#" className='text-slate-900 pb-6'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, 
+            aut molestiae velit nemo dolores facere molestias voluptatum 
+            distinctio magni eius praesentium, reiciendis ipsa officiis 
+            odit error, magnam perferendis ipsum iste.
+          </Link>
+          <Link href="#" className='text-pink-800 hover:text-black'>
+            続きを読む
+          </Link>
+        </div>
+      </article>
+      <article className='shadow my-4 flex flex-col'>
+        <Link href="#" className='hover:opacity-75'>
+          <Image
+            src="https://picsum.photos/1000/500?sig=2"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className='bg-white flex flex-col justify-start p-6'>
+          <Link href="#" className='text-blue-700 pb-4 font-bold'>
+            Technology
+          </Link>
+          <Link
+            href="#" 
+            className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className='text-sm pb-3 text-slate-900'>Published on 2023/07/15</p>
+          <Link href="#" className='text-slate-900 pb-6'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, 
+            aut molestiae velit nemo dolores facere molestias voluptatum 
+            distinctio magni eius praesentium, reiciendis ipsa officiis 
+            odit error, magnam perferendis ipsum iste.
+          </Link>
+          <Link href="#" className='text-pink-800 hover:text-black'>
+            続きを読む
+          </Link>
+        </div>
       </article>
     </div>
   )
