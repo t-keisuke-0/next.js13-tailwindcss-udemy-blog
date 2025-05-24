92a741118a6daff27f7e02b42ec146c5568c9791
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-24 17:11:23 +0900

    記事の削除

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,3 +1,4 @@
+import DeleteButton from '@/app/components/DeleteButton';
 import { getDetailArticles } from '@/blogAPI';
 import Image from 'next/image';
 import React from 'react'
@@ -5,6 +6,8 @@ import React from 'react'
 const Article = async ({ params }: { params: { id: string } }) => {
   const { id } = await params
   const detailArticles = await getDetailArticles(id);
+
+  const handleDelete = async () => {};
 
   return (
     <div className='max-w-3xl mx-auto p-5'>
@@ -17,6 +20,9 @@ const Article = async ({ params }: { params: { id: string } }) => {
         <h1 className='text-4xl text-center mb-10 mt-10'>{detailArticles.title}</h1>
         <div className='text-lg leading-relaxed text-justify'>
             <p>{detailArticles.content}</p>
+        </div>
+        <div className='text-right mt-3'>
+          <DeleteButton id={id} />
         </div>
     </div>
   )
